2efb2395cd7f2726f7c005dbb4363c55b2d7be69
robert.schneider <robert.schneider@stackpath.com>
2022-02-14 10:08:21 -0500

    Replaces with format print

--- main.go
+++ main.go
@@ -4,8 +4,11 @@ import "fmt"
 
 func main() {
 	var conferenceName = "Go Conference"
+	const conferenceTickets = 50
+	var remainingTickets = 50
 
-	fmt.Println("Welcome to", conferenceName, "booking application")
-	fmt.Println("Get your tickets here to attend")
+	fmt.Printf("Welcome to %v booking application\n", conferenceName)
+	fmt.Printf("We have a total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
+	fmt.Println("Get your tickets to attend")
 
 }
